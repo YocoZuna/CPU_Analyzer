aa984671fad9bc37bba4096b0774e0487bcd7cdd
YocoZuna <111345729+YocoZuna@users.noreply.github.com>
2023-06-09 12:12:46 +0200

    Addin main and Cmake

---
+++ build/CMakeFiles/CPU_Analyzer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPU_Analyzer.dir/main.c.obj"
+  "CMakeFiles/CPU_Analyzer.dir/main.c.obj.d"
+  "CPU_Analyzer.exe"
+  "CPU_Analyzer.exe.manifest"
+  "CPU_Analyzer.pdb"
+  "libCPU_Analyzer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CPU_Analyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CPU_Analyzer.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for CPU_Analyzer.
+# This may be replaced when dependencies are built.
